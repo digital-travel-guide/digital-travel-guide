42ed9ad5564de840c53a2a7b7a5c52020144f97a
regalado <ignacio.regalado295@gmail.com>
2018-03-15 20:11:50 -0700

    Added splash screen with countdown timer

---
+++ app/src/main/java/io/github/digital_travel_guide/digitaltravelguide/splashActivity.java
@@ -0,0 +1,37 @@
+package io.github.digital_travel_guide.digitaltravelguide;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //final ProgressBar loadingBar = (ProgressBar) findViewById(R.id.loadingBar);
+        final int maxTime = 3000;
+        //loadingBar.setMax(maxTime);
+        //loadingBar.setProgress(0, true);
+
+        //A count down timer that is done in milliseconds. So recall that 1000 milliseconds = 1 second
+        //This countdown is counting down from 3 seconds at intervals of 1 second.
+        new CountDownTimer(maxTime, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                //loadingBar.setProgress((int) (maxTime - millisUntilFinished), true);
+            }
+
+            public void onFinish() {
+                //loadingBar.setProgress(maxTime, true);
+                Intent i = new Intent(getApplicationContext(), Las_Vegas_MapActivity.class);
+                startActivity(i);
+            }
+        }.start();
+
+    }
+}
