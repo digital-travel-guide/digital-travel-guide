119e10655b037227ca962c4dba232c975da04d4c
Freedom Garcia <garcif2@unlv.nevada.edu>
2018-03-23 21:02:23 -0700

    Added location JSON Started adding loading locations from JSON

---
+++ app/src/main/java/Globals.java
@@ -0,0 +1,9 @@
+import android.app.Application;
+
+/**
+ * Created by Joel on 3/23/2018.
+ */
+
+public class Globals extends Application {
+
+}
--- app/src/main/java/io/github/digital_travel_guide/digitaltravelguide/splashActivity.java
+++ app/src/main/java/io/github/digital_travel_guide/digitaltravelguide/splashActivity.java
@@ -6,6 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class splashActivity extends AppCompatActivity {
     boolean initialStart = true;
 
@@ -18,6 +25,8 @@ public class splashActivity extends AppCompatActivity {
         final int maxTime = 3000;
         //loadingBar.setMax(maxTime);
         //loadingBar.setProgress(0, true);
+
+        loadLocationArray();
 
         //A count down timer that is done in milliseconds. So recall that 1000 milliseconds = 1 second
         //This countdown is counting down from 3 seconds at intervals of 1 second.
@@ -58,5 +67,58 @@ public class splashActivity extends AppCompatActivity {
                 }
             }.start();
         }
+    }
+
+    private void loadLocationArray() {
+        String jsonString = loadLocationJSON();
+
+        //load location info from json file
+        //store list/array as a global
+        /*
+        try {
+            JSONObject obj = new JSONObject(jsonString);
+            JSONArray m_jArry = obj.getJSONArray("formules");
+            ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> m_li;
+
+            for (int i = 0; i < m_jArry.length(); i++) {
+                JSONObject jo_inside = m_jArry.getJSONObject(i);
+                Log.d("Details-->", jo_inside.getString("formule"));
+                String formula_value = jo_inside.getString("formule");
+                String url_value = jo_inside.getString("url");
+
+                //Add your values in your `ArrayList` as below:
+                m_li = new HashMap<String, String>();
+                m_li.put("formule", formula_value);
+                m_li.put("url", url_value);
+
+                formList.add(m_li);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        */
+    }
+
+    private String loadLocationJSON() {
+        return loadJSONFromAsset("locations.json");
+    }
+
+    public String loadJSONFromAsset(String fileStr) {
+        String json = null;
+        /*
+        try {
+            InputStream is = getActivity().getAssets().open(fileStr);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        */
+        return json;
     }
 }
